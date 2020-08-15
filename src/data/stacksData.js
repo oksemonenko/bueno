1e736c5cfc45646f759b8cc46a2a1016fc51c4d8
Oksana <ok.semonenko@gmail.com>
2020-08-15 20:12:34 +0300

    add data

--- src/data/stacksData.js
+++ src/data/stacksData.js
@@ -1,242 +1,207 @@
+import Developer1 from '../img/type1/Developer1.jpg';
+import Developer2 from '../img/type1/Developer2.JPG';
+import Developer3 from '../img/type1/Developer3.JPG';
+import Developer4 from '../img/type1/Developer4.JPG';
+import Developer5 from '../img/type1/Developer5.JPG';
+import Developer6 from '../img/type1/Developer6.JPG';
+import Developer7 from '../img/type1/Developer7.jpg';
+import Dress1 from '../img/type2/Dress1.JPG';
+import Dress2 from '../img/type2/Dress2.JPG';
+import Dress3 from '../img/type2/Dress3.JPG';
+import Dress4 from '../img/type2/Dress4.JPG';
+import Dress5 from '../img/type2/Dress5.JPG';
+import Dress6 from '../img/type2/Dress6.JPG';
+import Dress7 from '../img/type2/Dress7.JPG';
+import Dress8 from '../img/type2/Dress8.JPG';
+import Dress9 from '../img/type2/Dress9.JPG';
+import Dancer1 from '../img/type3/Dancer1.jpg';
+import Dancer2 from '../img/type3/Dancer2.jpg';
+import Dancer3 from '../img/type3/Dancer3.jpg';
+import Dancer4 from '../img/type3/Dancer4.jpg';
+import Dancer5 from '../img/type3/Dancer5.jpg';
+import Dancer6 from '../img/type3/Dancer6.jpg';
+import Dancer7 from '../img/type3/Dancer7.jpg';
+import Dancer8 from '../img/type3/Dancer8.jpg';
+import Dancer9 from '../img/type3/Dancer9.jpg';
+import Dancer10 from '../img/type3/Dancer10.jpg';
+
 const stacksData = [
   {
-    title: '',
+    title: 'Developer',
     index: 0,
     items: [
       {
         index: 0,
-        // image: ,
+        image: Developer1,
         name: '',
         description: '',
 
       },
       {
         index: 1,
-        // image: ,
+        image: Developer2,
         name: '',
         description: '',
       },
       {
         index: 2,
-        // image: ,
+        image: Developer3,
         name: '',
         description: '',
       },
       {
         index: 3,
-        // image: ,
+        image: Developer4,
         name: '',
         description: '',
 
       },
       {
         index: 4,
-        // image: ,
+        image: Developer5,
         name: '',
         description: '',
 
       },
       {
         index: 5,
-        // image: ,
+        image: Developer6,
         name: '',
         description: '',
-
+      },
+      {
+        index: 6,
+        image: Developer7,
+        name: '',
+        description: '',
       },
     ]
   },
   {
-    title: '',
+    title: 'Dress maker',
     index: 1,
     items: [
       {
         index: 0,
-        // image: ,
+        image: Dress1,
         name: '',
         description: '',
       },
       {
         index: 1,
-        // image: ,
+        image: Dress2,
         name: '',
         description: '',
       },
       {
         index: 2,
-        // image: ,
+        image: Dress3,
         name: '',
         description: '',
       },
       {
         index: 3,
-        // image: ,
+        image: Dress4,
         name: '',
         description: '',
       },
       {
         index: 4,
-        // image: ,
+        image: Dress5,
+        name: '',
+        description: '',
+      },
+      {
+        index: 5,
+        image: Dress6,
+        name: '',
+        description: '',
+      },
+      {
+        index: 6,
+        image: Dress7,
+        name: '',
+        description: '',
+      },
+      {
+        index: 7,
+        image: Dress8,
+        name: '',
+        description: '',
+      },
+      {
+        index: 8,
+        image: Dress9,
         name: '',
         description: '',
       },
     ]
   },
   {
-    title: '',
+    title: 'Dancer',
     index: 2,
     items: [
       {
         index: 0,
-        // image: ,
+        image: Dancer1,
         name: '',
         description: '',
       },
       {
         index: 1,
-        // image: ,
+        image: Dancer2,
         name: '',
         description: '',
       },
       {
         index: 2,
-        // image: ,
+        image: Dancer3,
         name: '',
         description: '',
       },
       {
         index: 3,
-        // image: ,
-        name: '',
-        description: '',
-      },
-    ]
-  },
-  {
-    title: '',
-    index: 3,
-    items: [
-      {
-        index: 0,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 1,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 2,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 3,
-        // image: ,
+        image: Dancer4,
         name: '',
         description: '',
       },
       {
         index: 4,
-        // image: ,
+        image: Dancer5,
+        name: '',
+        description: '',
+      },
+      {
+        index: 5,
+        image: Dancer6,
+        name: '',
+        description: '',
+      },
+      {
+        index: 6,
+        image: Dancer7,
+        name: '',
+        description: '',
+      },
+      {
+        index: 7,
+        image: Dancer8,
+        name: '',
+        description: '',
+      },
+      {
+        index: 8,
+        image: Dancer9,
+        name: '',
+        description: '',
+      },
+      {
+        index: 9,
+        image: Dancer10,
         name: '',
         description: '',
       },
     ]
-  },
-  {
-    title: '',
-    index: 4,
-    items: [
-      {
-        index: 0,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 1,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 2,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 3,
-        // image: ,
-        name: '',
-        description: '',
-      },
-    ],
-  },
-  {
-    title: '',
-    index: 5,
-    items: [
-      {
-        index: 0,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 1,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 2,
-        // image: ,
-        name: '' +
-          '',
-        description: '',
-      },
-      {
-        index: 3,
-        // image: ,
-        name: '',
-        description: '',
-      },
-    ],
-  },
-  {
-    title: '',
-    index: 6,
-    items: [
-      {
-        index: 0,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 1,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 2,
-        // image: ,
-        name: '',
-        description: '',
-      },
-      {
-        index: 3,
-        // image: ,
-        name: '',
-        description: '',
-      },
-    ],
   },
 ];
 
