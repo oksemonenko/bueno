a007dd0a2325f90bc3e2d24a7c77177f41c8a267
Oksana <ok.semonenko@gmail.com>
2020-08-20 18:02:49 +0300

    image min

--- src/data/stacksData.js
+++ src/data/stacksData.js
@@ -3,27 +3,16 @@ import Developer2 from '../img/type1/Developer2.JPG';
 import Developer3 from '../img/type1/Developer3.JPG';
 import Developer4 from '../img/type1/Developer4.JPG';
 import Developer5 from '../img/type1/Developer5.JPG';
-import Developer6 from '../img/type1/Developer6.JPG';
-import Developer7 from '../img/type1/Developer7.jpg';
 import Dress1 from '../img/type2/Dress1.JPG';
 import Dress2 from '../img/type2/Dress2.JPG';
 import Dress3 from '../img/type2/Dress3.JPG';
 import Dress4 from '../img/type2/Dress4.JPG';
 import Dress5 from '../img/type2/Dress5.JPG';
-import Dress6 from '../img/type2/Dress6.JPG';
-import Dress7 from '../img/type2/Dress7.JPG';
-import Dress8 from '../img/type2/Dress8.JPG';
-import Dress9 from '../img/type2/Dress9.JPG';
 import Dancer1 from '../img/type3/Dancer1.jpg';
 import Dancer2 from '../img/type3/Dancer2.jpg';
 import Dancer3 from '../img/type3/Dancer3.jpg';
 import Dancer4 from '../img/type3/Dancer4.jpg';
 import Dancer5 from '../img/type3/Dancer5.jpg';
-import Dancer6 from '../img/type3/Dancer6.jpg';
-import Dancer7 from '../img/type3/Dancer7.jpg';
-import Dancer8 from '../img/type3/Dancer8.jpg';
-import Dancer9 from '../img/type3/Dancer9.jpg';
-import Dancer10 from '../img/type3/Dancer10.jpg';
 
 const stacksData = [
   {
@@ -63,18 +52,6 @@ const stacksData = [
         description: '',
 
       },
-      {
-        index: 5,
-        image: Developer6,
-        name: '',
-        description: '',
-      },
-      {
-        index: 6,
-        image: Developer7,
-        name: '',
-        description: '',
-      },
     ]
   },
   {
@@ -111,30 +88,6 @@ const stacksData = [
         name: '',
         description: '',
       },
-      {
-        index: 5,
-        image: Dress6,
-        name: '',
-        description: '',
-      },
-      {
-        index: 6,
-        image: Dress7,
-        name: '',
-        description: '',
-      },
-      {
-        index: 7,
-        image: Dress8,
-        name: '',
-        description: '',
-      },
-      {
-        index: 8,
-        image: Dress9,
-        name: '',
-        description: '',
-      },
     ]
   },
   {
@@ -168,36 +121,6 @@ const stacksData = [
       {
         index: 4,
         image: Dancer5,
-        name: '',
-        description: '',
-      },
-      {
-        index: 5,
-        image: Dancer6,
-        name: '',
-        description: '',
-      },
-      {
-        index: 6,
-        image: Dancer7,
-        name: '',
-        description: '',
-      },
-      {
-        index: 7,
-        image: Dancer8,
-        name: '',
-        description: '',
-      },
-      {
-        index: 8,
-        image: Dancer9,
-        name: '',
-        description: '',
-      },
-      {
-        index: 9,
-        image: Dancer10,
         name: '',
         description: '',
       },
